7a467204ecbbf9d0096aed44b202dd8c6514b8f0
Lauren Balch <llbalch@gmail.com>
2025-05-12 11:42:59 -0500

    created .env file and getImage function

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 require('dotenv').config();
 
+//request url
+const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=dogs&limit=25&offset=0&rating=g&lang=en`
+
+// query images
+async function getImages(query) {
+    const request = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`);
+    const data = await request.json();
+    let gifURL = data.data[0].images.original.url
+    console.log(gifURL);
+    return gifURL;
+}
+
+getImages('dogs');
+
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
