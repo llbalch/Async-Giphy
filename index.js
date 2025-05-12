3b239998dc3ea2cf5d466daf96b8d691445609e0
Lauren Balch <llbalch@gmail.com>
2025-05-12 12:18:34 -0500

    Extension: get random img from endpoint

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API
 
 // query images
 async function getImages(query) {
-    const request = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`);
-    const data = await request.json();
-    let gifURL = data.data[0].images.original.url
-    console.log(gifURL);
-    return gifURL;
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`);
+    const data = await response.json();
+    const gifs = json.data
+    const randomIndex = Math.floor(Math.random() * gifs.length);
+    const randomGif = gifs[randomIndex].images.original.url
+    return randomGif
 }
 
-getImages('dogs');
+async function helperFunction(){
+ await getImages('dogs');
+}
+
 
 // Print out value of API key stored in .env file
 console.log(process.env.API_KEY)
