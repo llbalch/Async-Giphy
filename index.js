389a924817d186c9f1be95f58d42947e1580aafc
Lauren Balch <llbalch@gmail.com>
2025-05-12 12:30:32 -0500

    added allGifs array to index.js

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ async function getImages(query) {
     const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`);
     const data = await response.json();
     const gifs = json.data
+    const allGifs = gifs.map(gif => gif.images.original.url);
     const randomIndex = Math.floor(Math.random() * gifs.length);
     const randomGif = gifs[randomIndex].images.original.url
     return randomGif
